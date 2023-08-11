b3cb900f6d68bdd6a5ebb68a36a99842dfc461b1
Pikachú <engbethany4@gmail.com>
2023-08-11 11:52:41 +0300

    API gives error 400, will fix later

--- app.py
+++ app.py
@@ -4,40 +4,45 @@ from flask import Flask, request, jsonify, render_template
 import os
 import openai
 
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 # Set up OpenAI API credentials
 openai.api_type = "azure"
 openai.api_base = "https://test-onetwothree-openai.openai.azure.com/"
 openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ['OPENAI_API_KEY']
+                            
+@app.route("/", methods=["GET"])
+def home():
+    # Render the index.html template
+    return render_template("index.html")
 
 # Define a route for handling incoming chat messages
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        # Get the message from the request body
-        message = request.form['text']
+    # Get the message from the request body
+    message = request.form['text']
 
-        # Call the OpenAI API to generate a response
-        response = openai.ChatCompletion.create(
-            engine="newturbs",
-            prompt=message,
-            temperature=0.7,
-            max_tokens=800,
-            top_p=0.95,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=None
-        )
+    # Call the OpenAI API to generate a response
+    response = openai.ChatCompletion.create(
+        engine="newturbs",
+        prompt=message,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
 
-        # Extract the response text from the API response
-        response_text = response.choices[0].text.strip()
+    # Extract the response text from the API response
+    response = response.choices[0].text.strip()
 
-            # Return the response as a JSON object
-        return jsonify({'response': response_text})
-    else:
-        return render_template('index.html')
+    # Return the response as a JSON object
+    return jsonify({'response': response})
 
 if __name__ == '__main__':
     app.run(debug=True)
